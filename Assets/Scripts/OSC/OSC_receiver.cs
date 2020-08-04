7068e983a6c44f8392f9b558795162114c06178a
doremi31618 <doremi31618@gmail.com>
2020-08-04 10:31:25 +0800

    first commit

---
+++ Assets/Scripts/OSC/OSC_receiver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OscJack;
+
+public class OSC_receiver : MonoBehaviour
+{
+    public int UDP_port_number = 9000;
+    public string osc_address="/test";
+
+    IEnumerator Start(){
+        var server = new OscServer(UDP_port_number);
+        
+        server.MessageDispatcher.AddCallback(
+            "/test", // OSC address
+            (string address, OscDataHandle data) => {
+                Debug.Log(string.Format("({0}, {1})",
+                    data.GetElementAsFloat(0),
+                    data.GetElementAsString(0)));
+            }
+        );
+
+        print("on call");
+        yield return new WaitForSeconds(10);
+        server.Dispose();
+    }
+}
---
+++ Assets/Scripts/OSC/OSC_sender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OscJack;
+public class OSC_sender : MonoBehaviour
+{
+    [Header("IP information")]
+    public string ip_address="127.0.0.1";
+    public int UDP_port_number = 9000;
+    [Header("OSC information")]
+    public string osc_address="/test";
+    public string content="osc_content";
+
+    
+    IEnumerator Start(){
+        var client  = new OscClient(ip_address, UDP_port_number);
+        for(var i=0; i<10; i++){
+            yield return new WaitForSeconds(0.5f);
+            client.Send(osc_address,
+                        i*10.0f);
+            client.Send(osc_address,
+                        content);
+        }
+        
+        client.Dispose();
+    }
+}
