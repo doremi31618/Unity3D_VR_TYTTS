7f336950a2c16d9410dcbbf09ea044bfd472399c
Aaron <her8153101@yahoo.com.tw>
2020-08-07 15:52:46 +0800

    Setup wind effect.
    1. Add WindEffect_test scene.
    2. Add Wind prefab.
    3. Add WindControl.cs to control wind produce.
    4. Add Player.cs to move obj as a tester.

---
+++ Assets/Scripts/WindEffect/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeReference]
+    float speed;
+    Vector3 movement;
+
+    Vector3 GetDirection()
+    {
+        Vector3 direction = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+        return direction * Time.deltaTime * speed;
+    }
+
+    void Update()
+    {
+        transform.Translate(GetDirection());
+    }
+}
