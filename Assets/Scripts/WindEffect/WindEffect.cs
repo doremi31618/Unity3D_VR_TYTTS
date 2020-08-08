f3935a21c4e7ba0189454811d80f2261a5caa97b
Aaron <her8153101@yahoo.com.tw>
2020-08-09 05:10:53 +0800

    Refactor Wind Effect.
    1. Add new Wind Effects, Start and Lead Effect.
    2. Add Wind Control Gameobject to control Wind Effect.
    3. Refactor WindEffect.cs to control Wind Effect.
    
    P.S.
    1. Press "space" to change wind state.
    2. Setup "public void ChangeState()" to call.
    3. Custom wind effects radius are "both" able to change.

---
+++ Assets/Scripts/WindEffect/WindEffect.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffect : MonoBehaviour
+{
+    [SerializeField]
+    Transform playerTransform;
+
+    bool IsBeginning = true;
+
+    [SerializeField]
+    ParticleSystem startWind;
+    [SerializeField]
+    ParticleSystem leadWind;
+
+    [SerializeField]
+    float height;
+    [SerializeField]
+    float beginningLoopTime = 1f;
+    [SerializeField]
+    float beginningStartTime = 0f;
+
+    [SerializeField]
+    float radius;
+    [SerializeField]
+    float loopTime = 1f;
+    [SerializeField]
+    float startTime = 0f;
+
+    public void ChangeState()
+    {
+        CancelInvoke();
+        if (IsBeginning)
+        {
+            leadWind.gameObject.SetActive(true);
+            startWind.gameObject.SetActive(false);
+            InvokeRepeating("AutoPlayLeadWind", startTime, loopTime);
+        }
+        else
+        {
+            leadWind.gameObject.SetActive(false);
+            startWind.gameObject.SetActive(true);
+            InvokeRepeating("AutoPlayStartWind", beginningStartTime, beginningLoopTime);
+        }
+        IsBeginning = !IsBeginning;
+        
+    }
+
+    void AutoPlayLeadWind()
+    {
+        float newPosx = Random.Range(-radius, radius);
+        float newPosy = Random.Range(-radius, radius);
+        leadWind.gameObject.transform.position = playerTransform.position + new Vector3(newPosx,
+                                                                    newPosy,
+                                                                    0);
+        leadWind.Play(true);
+    }
+    void AutoPlayStartWind()
+    {
+        float newPosy = Random.Range(0, height);
+        startWind.gameObject.transform.position = playerTransform.position + new Vector3(startWind.transform.position.x,
+                                                                                         newPosy,
+                                                                                         0);
+        startWind.Play(true);
+    }
+    private void Start()
+    {
+        startWind.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        leadWind.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        InvokeRepeating("AutoPlayStartWind", beginningStartTime, beginningLoopTime);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeState();
+        }
+    }
+}
